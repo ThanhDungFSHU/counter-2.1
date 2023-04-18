0828fb8b7a6409b5bd74af95c33650f7bda12245
ThanhDungFSHU <119802540+ThanhDungFSHU@users.noreply.github.com>
2023-04-19 03:12:30 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(100);
+  const [count, setCount] = useState(0);
   const increment = () => setCount(count + 1);
   const decrement = () => setCount(count - 1);
   const reset = () => setCount(0);
